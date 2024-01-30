a5e5faa8a124827076936a1c5c2335ca90d03d6a
Rumcajs121 <z.miara45@gmail.com>
2024-01-30 17:56:27 +0100

    30.10.2024 - I start prepare my frontend side web

--- BookShop.Web/Services/BookServices.cs
+++ BookShop.Web/Services/BookServices.cs
@@ -16,32 +16,11 @@ namespace BookShop.Web.Services
 
         public async Task<List<BookDto>> GetAll()
         {
-            try
-            {
-                var response = await _httpClient.GetAsync("BookShop/GetAll");
-                response.EnsureSuccessStatusCode();
-
-                var content = await response.Content.ReadAsStringAsync();
-                var books = JsonConvert.DeserializeObject<List<BookDto>>(content);
-
-                // Dodaj wypisywanie informacji do konsoli
-                Debug.WriteLine("Pomyślnie pobrano dane z API.");
-
-                return books;
-            }
-            catch (Exception ex)
-            {
-                // Dodaj wypisywanie informacji do konsoli
-                Debug.WriteLine($"Błąd podczas pobierania danych z API: {ex.Message}");
-
-                // Rzuć ponownie błąd
-                throw;
-            }
-            //var response = await _httpClient.GetAsync("GetAll");
-            //response.EnsureSuccessStatusCode();
-            //var content = await response.Content.ReadAsStringAsync();
-            //var books = JsonConvert.DeserializeObject<List<BookDto>>(content);
-            //return books;
+            var response = await _httpClient.GetAsync("BookShop/GetAll");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var books = JsonConvert.DeserializeObject<List<BookDto>>(content);
+            return books;
         }
     }
 }
